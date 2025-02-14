2bc6cda94269ba621f12982e50fab30ee316d122
Eliya Evra <45659796+eliyaevra@users.noreply.github.com>
2025-02-14 12:25:30 +0300

    Create borrowing.rb

---
+++ app/models/borrowing.rb
@@ -0,0 +1,22 @@
+class Borrowing < ApplicationRecord
+  belongs_to :user
+  belongs_to :book
+
+  validates :user, presence: true
+  validates :book, presence: true
+  validate :book_not_already_borrowed
+
+  before_create :set_due_date
+
+  private
+
+  def book_not_already_borrowed
+    if book.borrowings.where(returned_at: nil).exists?
+      errors.add(:book, "is already borrowed")
+    end
+  end
+
+  def set_due_date
+    self.due_date = 2.weeks.from_now
+  end
+end
